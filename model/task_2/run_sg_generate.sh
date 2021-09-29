674269654ce5294bc98b0008d6f0acc6d88ed288
Bhathiya Supun <hsb2000@hw.ac.uk>
2021-09-30 04:11:08 +0530

    from script changes

---
+++ model/task_2/run_sg_generate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ $# -lt 1 ]]
+then
+    PATH_DIR=$(realpath .)
+else
+    PATH_DIR=$(realpath "$1")
+fi
+
+# Generate sentences (Furniture, multi-modal)
+python -m gat_coref.scripts.graph_generate.task2_scene_graph_generation \
+--sg_output="${PATH_DIR}"/gat_coref/data/dst_coref/graph_data/scene_graph.json  \
+--fashion_json_input="${PATH_DIR}"/../../data/fashion_prefab_metadata_all.json \
+--furniture_json_input="${PATH_DIR}"/../../data/furniture_prefab_metadata_all.json \
