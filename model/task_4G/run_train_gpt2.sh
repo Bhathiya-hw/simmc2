602bc66b8c1ac0c83b35099cfc62f649247f8111
Bhathiya Supun <hsb2000@hw.ac.uk>
2021-10-01 01:51:55 +0530

    task-4 generation

---
+++ model/task_4G/run_train_gpt2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [[ $# -lt 1 ]]
+then
+    PATH_DIR=$(realpath .)
+    echo $PATH_DIR
+else
+    PATH_DIR=$(realpath "$1")
+    echo $PATH_DIR
+fi
+
+# Train (multi-modal)
+python3 -m generation.scripts.run_language_modeling \
+  --output_dir="${PATH_DIR}"/generation/save/with_gat  \
+  --model_type=gpt2 \
+  --model_name_or_path=gpt2 \
+  --line_by_line \
+  --add_special_tokens="${PATH_DIR}"/generation/data/simmc2_special_tokens.json \
+  --do_train  \
+  --train_data_file="${PATH_DIR}"/generation/data/simmc2_dials_dstc10_train_target.txt \
+  --predict_train_data_file="${PATH_DIR}"/generation/data/simmc2_dials_dstc10_train_predict.txt \
+  --scene_train_data_file="${PATH_DIR}"/generation/data/simmc2_dials_dstc10_train_scene.txt  \
+  --belief_train_data_file="${PATH_DIR}"/generation/data/simmc2_dials_dstc10_train_sys_belief.txt  \
+  --graph_json_file="${PATH_DIR}"/generation/data/graph_data/scene_graph.json \
+  --do_eval \
+  --eval_data_file="${PATH_DIR}"/generation/data/simmc2_dials_dstc10_devtest_target.txt  \
+  --num_train_epochs=5  \
+  --overwrite_output_dir  \
+  --per_gpu_train_batch_size=2  \
+  --per_gpu_eval_batch_size=1 \
+  --save_steps=10 \
+  --gat_conv_layers=2\
+  --no_cuda
+
