e0948b47772d103089f12e572dce9d7d070c07f3
Bhathiya-hw <hsb2000@hw.ac.uk>
2021-10-02 16:24:29 +0100

    2 stage approach

--- model/task_2/run_preprocess_gpt2.sh
+++ model/task_2/run_preprocess_gpt2.sh
@@ -11,33 +11,32 @@ fi
 # Train split
 python3 -m gat_coref.scripts.preprocess_input \
     --input_path_json="${PATH_DATA_DIR}"/simmc2_dials_dstc10_train.json \
-    --input_path_sg="${PATH_DIR}"/gat_coref/data/dst_coref/graph_data/scene_graph.json \
     --output_path_predict="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_train_predict.txt \
     --output_path_target="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_train_target.txt \
     --len_context=2 \
     --use_multimodal_contexts=1 \
     --output_path_special_tokens="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_special_tokens.json \
-    --output_path_scene="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_train_scene.txt
-
+#    --output_path_scene="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_train_scene.txt
+#    --input_path_sg="${PATH_DIR}"/gat_coref/data/dst_coref/graph_data/scene_graph.json \
 # Dev split
 python3 -m gat_coref.scripts.preprocess_input \
     --input_path_json="${PATH_DATA_DIR}"/simmc2_dials_dstc10_dev.json \
-    --input_path_sg="${PATH_DIR}"/gat_coref/data/dst_coref/graph_data/scene_graph.json \
     --output_path_predict="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_dev_predict.txt \
     --output_path_target="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_dev_target.txt \
     --len_context=2 \
     --use_multimodal_contexts=1 \
     --input_path_special_tokens="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_special_tokens.json \
     --output_path_special_tokens="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_special_tokens.json \
-    --output_path_scene="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_dev_scene.txt
+#    --output_path_scene="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_dev_scene.txt
+#    --input_path_sg="${PATH_DIR}"/gat_coref/data/dst_coref/graph_data/scene_graph.json \
 # Devtest split
 python3 -m gat_coref.scripts.preprocess_input \
     --input_path_json="${PATH_DATA_DIR}"/simmc2_dials_dstc10_devtest.json \
-    --input_path_sg="${PATH_DIR}"/gat_coref/data/dst_coref/graph_data/scene_graph.json \
     --output_path_predict="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_devtest_predict.txt \
     --output_path_target="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_devtest_target.txt \
     --len_context=2 \
     --use_multimodal_contexts=1 \
     --input_path_special_tokens="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_special_tokens.json \
     --output_path_special_tokens="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_special_tokens.json \
-    --output_path_scene="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_devtest_scene.txt
+#    --output_path_scene="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_devtest_scene.txt
+#    --input_path_sg="${PATH_DIR}"/gat_coref/data/dst_coref/graph_data/scene_graph.json \
--- model/task_2/run_train_gpt2.sh
+++ model/task_2/run_train_gpt2.sh
@@ -8,39 +8,23 @@ else
     echo $PATH_DIR
 fi
 
---output_dir
-/home/bhash/GAT2GPT/simmc2/model/task_2/gat_coref/save/task23/taskDC_mock
---model_type
-gpt2
---model_name_or_path
-gpt2
---line_by_line
---add_special_tokens
-/home/bhash/GAT2GPT/simmc2/model//task_2/gat_coref/data/dst_coref/simmc2_special_tokens.json
-
-
-
-
 # Train (multi-modal)
 python3 -m gat_coref.scripts.run_language_modeling \
-  --output_dir="${PATH_DIR}"/gat_coref/save/dc_g4  \
+  --output_dir="${PATH_DIR}"/gat_coref/save/baseline_pre-trained  \
   --model_type=gpt2 \
   --model_name_or_path=gpt2 \
   --line_by_line \
   --add_special_tokens="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_special_tokens.json \
   --do_train  \
-  --train_data_file="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_dev_target.txt \
-  --predict_train_data_file="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_dev_predict.txt \
-  --scene_train_data_file="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_dev_scene.txt \
+  --train_data_file="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_train_target.txt \
+  --predict_train_data_file="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_train_predict.txt \
+  --scene_train_data_file="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_train_scene.txt \
   --graph_json_file="${PATH_DIR}"/gat_coref/data/dst_coref/graph_data/scene_graph.json \
   --do_eval \
-  --eval_data_file="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_dev_target.txt  \
-  --num_train_epochs=2  \
+  --eval_data_file="${PATH_DIR}"/gat_coref/data/dst_coref/simmc2_dials_dstc10_train_target.txt  \
+  --num_train_epochs=5  \
   --overwrite_output_dir  \
   --per_gpu_train_batch_size=1  \
   --per_gpu_eval_batch_size=1 \
   --save_steps=5000 \
   --gat_conv_layers=4\
-
-
-
